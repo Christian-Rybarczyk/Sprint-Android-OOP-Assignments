9e2aa2a4f032b9bcdae16394128fd23e9605babf
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-09-09 15:59:47 -0400

    Added 3 subclasses

--- M01/Assignment/app/src/main/java/com/rybarstudios/oopassignment1/model/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/rybarstudios/oopassignment1/model/ShoppingItem.kt
@@ -1,9 +1,44 @@
 package com.rybarstudios.oopassignment1.model
 
-open class  ShoppingItem protected constructor(val colorId: Int, val productName: String) {
+open class  ShoppingItem protected constructor(val colorId: Int, val productName: String, val itemPrice: Int) {
 
-    fun getDisplayName(productName: String) : String {
-        return productName
+    open fun getDisplayName() : String {
+        return """
+            Product name:  $productName
+            Product price: $itemPrice
+        """.trimIndent()
     }
 }
 
+class GroceryItem(colorId: Int, productName: String, itemPrice: Int, val isRefigerated: Boolean): ShoppingItem(colorId, productName, itemPrice) {
+
+    override fun getDisplayName(): String {
+        return """
+            Product name:           $productName
+            Product price:          $itemPrice
+            Requires refrigeration: $isRefigerated
+        """.trimIndent()
+    }
+}
+
+class ClothingItem(colorId: Int, productName: String, itemPrice: Int, val size: String): ShoppingItem(colorId, productName, itemPrice) {
+
+    override fun getDisplayName(): String {
+        return """
+            Product name:  $productName
+            Product price: $itemPrice
+            Product size:  $size
+        """.trimIndent()
+    }
+}
+
+class SportsItem(colorId: Int, productName: String, itemPrice: Int, val typeOfSport: String): ShoppingItem(colorId, productName, itemPrice) {
+
+    override fun getDisplayName(): String {
+        return """
+            Product name:  $productName
+            Product price: $itemPrice
+            Sport:         $typeOfSport
+        """.trimIndent()
+    }
+}
