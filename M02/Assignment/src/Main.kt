4ce51f844d9ace3a36e29307f88051b8dbaf3534
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-09-10 15:48:25 -0400

    Added the 3 required interfaces

--- M02/Assignment/src/Main.kt
+++ M02/Assignment/src/Main.kt
@@ -1,4 +1,16 @@
 
 fun main(args: Array<String>) {
-    
+
+}
+
+interface AirTravel {
+    fun fly()
+}
+
+interface WaterTravel {
+    fun sail()
+}
+
+interface GroundTravel {
+    fun drive()
 }
